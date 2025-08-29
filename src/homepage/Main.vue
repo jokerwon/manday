21ec428442ba233f6009feead562a3a12b5db300
JokerWon <1595672103@qq.com>
2025-08-29 22:50:08 +0800

    perf: 优化间距

--- src/homepage/Main.vue
+++ src/homepage/Main.vue
@@ -3,7 +3,7 @@ import Search from './Search.vue'
 </script>
 
 <template>
-  <div class="relative w-fit flex flex-col items-center mt-32 px-10 py-12 pt-8 rounded-sm shadow-elevation bg-bg-primary transition-all duration-300">
+  <div class="relative w-fit flex flex-col items-center mt-[12vh] px-10 py-12 pt-8 rounded-sm shadow-elevation bg-bg-primary transition-all duration-300">
     <div class="absolute top-0 translate-y-[-50%] flex items-center justify-center p-2 rounded-full bg-secondary text-4xl text-[var(--color-jade-pale)] transition-all duration-300">
       <Logo />
     </div>
