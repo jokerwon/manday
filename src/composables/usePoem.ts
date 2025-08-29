6f4923f5273ec493d9f6e6e88a6a2d4533a2fd8f
Joker Won <1595672103@qq.com>
2025-08-29 14:24:40 +0800

    chore: turn on jinrishici

--- src/composables/usePoem.ts
+++ src/composables/usePoem.ts
@@ -1,17 +1,19 @@
-export function usePoem() {
-  // const { data } = useFetch<any>('https://v2.jinrishici.com/one.json').json()
+import { useFetch } from '@vueuse/core'
 
-  // return computed<any>(() => {
-  //   const d = data.value?.data
-  //   return {
-  //     content: d?.content,
-  //     title: d?.origin?.title,
-  //     author: d?.origin?.author,
-  //   }
-  // })
-  return {
-    content: '迟日江山丽，春风花草香。',
-    title: '绝句二首',
-    author: '杜甫',
-  }
+export function usePoem() {
+  const { data } = useFetch<any>('https://v2.jinrishici.com/one.json').json()
+
+  return computed<any>(() => {
+    const d = data.value?.data
+    return {
+      content: d?.content,
+      title: d?.origin?.title,
+      author: d?.origin?.author,
+    }
+  })
+  // return {
+  //   content: '迟日江山丽，春风花草香。',
+  //   title: '绝句二首',
+  //   author: '杜甫',
+  // }
 }
